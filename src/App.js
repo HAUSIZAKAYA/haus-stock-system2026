d4b21603f42588bf9a4f220793a2a216ea2de908
HAUSIZAKAYA <ronnachit19@gmail.com>
2026-03-07 19:40:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default function App() {
     setUser(u.toUpperCase()); // ล็อกอินชื่อไหน โชว์ชื่อนั้นเป๊ะๆ
     setIsLogin(true);
   };
+  
 
   if (!isLogin) return (
     <div style={styles.loginContainer}>
